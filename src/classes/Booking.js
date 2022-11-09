f2b367ed67ba8535c04a5bbb1e59eb5b9239f001
Sam Rice <samsrice@gmail.com>
2022-11-08 16:59:41 -0800

    add and import/export all class files

---
+++ src/classes/Booking.js
@@ -0,0 +1,5 @@
+class Booking {
+
+};
+
+module.exports = Booking;
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,6 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-
-console.log('This is the JavaScript entry file - your code begins here.');
+import Booking from '../src/classes/Booking'
+import Customer from '../src/classes/Customer'
+import Manager from '../src/classes/Manager'
+import Room from '../src/classes/Room'
