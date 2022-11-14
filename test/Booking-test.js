b59ffbbcbe59570294918e86030810bc524f3592
Sam Rice <samsrice@gmail.com>
2022-11-14 15:33:54 -0800

    add completed unit test for Booking class

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -13,4 +13,18 @@ describe('Booking Class', function() {
   it('should be an instance of the Booking Class', function() {
     expect(booking).to.be.an.instanceOf(Booking);
   });
+
+  it('should have properties for a booking ID, guest ID, date, and room number.', function() {
+    expect(booking).to.haveOwnProperty("id");
+    expect(booking).to.haveOwnProperty("guestId");
+    expect(booking).to.haveOwnProperty("date");
+    expect(booking).to.haveOwnProperty("roomNumber");
+  });
+
+  it('should have properties with properly-formatted values.', function() {
+    expect(booking.id).to.eql("5fwrgu4i7k55hl6sz");
+    expect(booking.guestId).to.eql(9);
+    expect(booking.date).to.eql("2022-04-22");
+    expect(booking.roomNumber).to.eql(15);
+  });
 });
